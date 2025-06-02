8922cba01729e9f44b23c164892511c0ac36b031
iceweasel <johnsjosephk@pm.me>
2025-06-02 13:30:45 +0530

    cookpedia basic files

---
+++ src/app/app.routes.ts
@@ -0,0 +1,20 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ViewRecipeComponent } from './view-recipe/view-recipe.component';
+import { Page404Component } from './page404/page404.component';
+
+export const routes: Routes = [
+    { path:'',component:HomeComponent },
+    { path:'register',component:RegisterComponent },
+    { path:'login',component:LoginComponent },
+    { path:'all-recipe',component:RecipeComponent },
+    { path:'about',component:AboutComponent },
+    { path:'contact',component:ContactComponent },
+    { path:'view/:id',component:ViewRecipeComponent },
+    { path:'**',component:Page404Component },
+];
