0be299c02a236335227531f2326902ed35e87480
iceweasel <johnsjosephk@pm.me>
2025-06-10 13:42:02 +0530

    piped introduction , recipe componentfilter

--- src/app/recipe/recipe.component.ts
+++ src/app/recipe/recipe.component.ts
@@ -1,21 +1,28 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { ApiService } from '../service/api.service';
+import { DatePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { SearchPipe } from '../pipes/search.pipe';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
   selector: 'app-recipe',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent,DatePipe,FormsModule,SearchPipe,NgxPaginationModule],
   templateUrl: './recipe.component.html',
   styleUrl: './recipe.component.css'
 })
 export class RecipeComponent {
   constructor(private api:ApiService){
-
   }
 
+  p:number = 1
+  time:Date = new Date()
+  searchKey:string = ''
   cuisineTypes:any = []
   AllRecipes:any = []
   allMealTypes:any  = []
+  dummyArray:any = []
 
   ngOnInit(){
     this.getAllRecipes();
@@ -26,7 +33,7 @@ export class RecipeComponent {
       next: (result:any) => {
         console.log(result)
         this.AllRecipes = result;
-
+        this.dummyArray = result;
         //cuisine and Meal Types
         result.forEach( (item:any) => {
 
@@ -49,5 +56,19 @@ export class RecipeComponent {
         console.log(error)
       }
     })
+  }
+
+  filterCuisine(item:any){
+    console.log(item);
+    this.AllRecipes = this.dummyArray.filter( (recipe:any) => recipe.cuisine == item )
+  }
+
+  filterMeals(item:any){
+    this.AllRecipes = this.dummyArray.filter( (recipe:any) =>
+       recipe.mealType.includes(item) )
+  }
+
+  viewallMeals(){
+    this.AllRecipes = this.dummyArray;
   }
 }
