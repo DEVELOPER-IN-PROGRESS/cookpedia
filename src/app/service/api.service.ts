e7b14df38c1e557f0f053e515041114ac4206e33
iceweasel <johnsjosephk@pm.me>
2025-08-15 00:20:51 +0530

    typo fix

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,4 +1,4 @@
-]import { HttpClient } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { HttpHeaderResponse, HttpHeaders } from '@angular/common/http';
 @Injectable({
